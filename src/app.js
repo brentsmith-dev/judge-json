e1f11275fab731d1428e6838668fee4cae9d9c2a
Brent Smith <brent.smith@redspace.com>
2017-11-24 21:19:46 -0400

    cleaning up app.js

--- src/app.js
+++ src/app.js
@@ -1,41 +1,28 @@
 var express = require('express')
 var bodyParser = require('body-parser');
-var judge =  require('./judge.js')
+var judge = require('./judge.js')
 
 var app = express();
-
-console.log('starting');
-
-// set the port of our application
-// process.env.PORT lets the port be set by Heroku
 var port = process.env.PORT || 8000;
 
-app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.use(function(err, req, res, next) {
-    if (err.status == 400) {
-      res.status(400).send('invalid JSON');
-    } else {
-      next(err);
-    }
-  });
+app.use(function (err, req, res, next) {
+	if (err.status == 400) {
+		res.status(400).send('invalid JSON');
+	} else {
+		next(err);
+	}
+});
 
 app.get('/', function (req, res) {
-	res.send('Welcome to my court!')
+	res.send('Welcome to court!')
 })
 
 app.post('/validate', function (req, res) {
 	judge.judgeData(req.body, res);
 });
 
-app.options('/validate', function( req, res) {
-	res.header('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
-	res.header('Access-Control-Allow-Headers', 'origin,content-type,accept');
-	res.header('Allow', 'OPTIONS,HEAD,POST,GET');
-	res.sendStatus(204) ;
-});
-
 app.listen(port, function () {
-	console.log('Example app listening on port ' + port)
+	console.log(`court is in session on port: ${port}`);
 })
